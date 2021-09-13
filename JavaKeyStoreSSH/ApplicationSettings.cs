ceeeb9e3de01c88b654f4337580f72d9e39d8875
leefine02 <50836957+leefine02@users.noreply.github.com>
2021-09-13 12:57:16 -0400

    Add 'Negotiate" option for Authentication
    Add Negotiate config option and update Renci.SSH.Net reference

--- JavaKeyStoreSSH/ApplicationSettings.cs
+++ JavaKeyStoreSSH/ApplicationSettings.cs
@@ -22,6 +22,7 @@ namespace JavaKeyStoreSSH
         public static bool UseSeparateUploadFilePath { get; set; }
         public static string SeparateUploadFilePath { get; set; }
         public static bool FindKeytoolPathOnWindows { get; set; }
+        public static bool UseNegotiateAuth { get; set; }
 
         public static void Initialize(string currLocation)
         {
@@ -56,6 +57,7 @@ namespace JavaKeyStoreSSH
             UseSeparateUploadFilePath = jsonContents.UseSeparateUploadFilePath.Value.Equals("Y", System.StringComparison.OrdinalIgnoreCase);
             SeparateUploadFilePath = AddTrailingSlash(jsonContents.SeparateUploadFilePath.Value);
             FindKeytoolPathOnWindows = jsonContents.FindKeytoolPathOnWindows.Value.Equals("Y", System.StringComparison.OrdinalIgnoreCase);
+            UseNegotiateAuth = jsonContents.UseNegotiateAuth.Value.Equals("Y", System.StringComparison.OrdinalIgnoreCase);
         }
 
         private static string AddTrailingSlash(string path)
@@ -81,6 +83,8 @@ namespace JavaKeyStoreSSH
                 errors += "SeparateUploadFilePath, ";
             if (jsonContents.FindKeytoolPathOnWindows == null)
                 errors += "FindKeytoolPathOnWindows, ";
+            if (jsonContents.UseNegotiateAuth == null)
+                errors += "UseNegotiateAuth, ";
 
             if (errors.Length > 0)
                 throw new JKSException($"The following configuration items are missing from the config.json file: {errors.Substring(0, errors.Length-2)}");
